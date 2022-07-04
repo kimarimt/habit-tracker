2d02f214835cf34949b9415aa6c55d996101c89f
Alec German <kimarimt@protonmail.com>
2022-07-04 18:49:35 -0400

    finished project

--- main.py
+++ main.py
@@ -1,5 +1,59 @@
+import requests
+import os
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+GRAPH_NAME = 'Meditation'
+GRAPH_ID = 'med-tracker'
+TOKEN = os.getenv('PIXELA_TOKEN')
+
+
+def create_graph():
+    graph_endpoint = f'https://pixe.la/v1/users/agdeveloper93/graphs'
+    headers = {
+        'X-USER-TOKEN': TOKEN,
+    }
+    config = {
+        'id': GRAPH_ID,
+        'name': GRAPH_NAME,
+        'unit': 'minutes',
+        'type': 'int',
+        'color': 'sora',
+    }
+
+    response = requests.post(
+        url=graph_endpoint,
+        headers=headers,
+        json=config
+    )
+
+    print(response.json())
+
+
+def update_point():
+    today = datetime.datetime.today().strftime('%Y%m%d')
+    update_graph = f'https://pixe.la/v1/users/agdeveloper93/graphs/{GRAPH_ID}'
+    headers = {
+        'X-USER-TOKEN': TOKEN
+    }
+    config = {
+        'date': today,
+        'quantity': '30'
+    }
+
+    response = requests.post(
+        url=update_graph,
+        headers=headers,
+        json=config
+    )
+
+    print(response.json())
+
+
 def main():
-    print('Hello, World!')
+    create_graph()
+    update_point()
 
 
 if __name__ == '__main__':
